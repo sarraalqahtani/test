e7fd6996d64a8537394fc568f77042407bb2b289
Alqahtani <sarra-alqahtani@wfu.edu>
2020-08-26 14:42:11 -0400

    update2

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         String string = "The best of both worlds";
         int count = 0;
-        for (int i = 0; i++) {
+        for (int i = 0; i<=10;i++ ) {
 
         }
         System.out.println("Total number of characters in a string: " + count);
