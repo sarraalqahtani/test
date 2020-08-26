63f8865a123c09e46c2f21fae4ad486fbb940fc3
Alqahtani <sarra-alqahtani@wfu.edu>
2020-08-26 14:30:07 -0400

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        String string = "The best of both worlds";
+        int count = 0;
+        for (int i = 0; i++) {
+
+        }
+        System.out.println("Total number of characters in a string: " + count);
+    }
+}
